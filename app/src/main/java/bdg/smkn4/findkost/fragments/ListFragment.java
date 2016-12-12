9fc41d12fedc292d512fca74411705b5d25fd7e7
setiawan <setiawangunadi04@gmail.com>
2016-12-12 14:26:22 +0700

    List Fragment diubah

--- app/src/main/java/bdg/smkn4/findkost/fragments/ListFragment.java
+++ app/src/main/java/bdg/smkn4/findkost/fragments/ListFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
 import bdg.smkn4.findkost.R;
 
@@ -65,7 +66,10 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_list, container, false);
+        RatingBar ratingBar = (RatingBar) v.findViewById(R.id.ratingList);
+        ratingBar.setRating(5);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
